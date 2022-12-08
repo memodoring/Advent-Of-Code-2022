848e0befef5ce8211576483f0aebfdf9e2752d25
Memo Doring <memodoring@users.noreply.github.com>
2022-12-07 19:54:58 -0800

    testing stuff

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,11 +7,17 @@ fn main() {
     let args:Vec<String> = env::args().collect();
     let file_path = &args[1];
     if let Ok(lines) =  read_lines(file_path){
-        if let Ok(input) = line {
-            println!("{}", input);
+        for line in lines {
+            if let Ok(input) = line {
+                if !input.is_empty(){
+                    println!("{}", input);
+                    let input_as_vec_chars: Vec<&char> = input.chars().collect();
+                }
+            }
         }
     }
 }
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
 where P: AsRef<Path>,{
     let file = File::open(filename)?;
---
+++ day6/target/debug/day6.d
@@ -0,0 +1,1 @@
+/Users/doringm/dev/2022adventofcode/day6/target/debug/day6: /Users/doringm/dev/2022adventofcode/day6/src/main.rs
