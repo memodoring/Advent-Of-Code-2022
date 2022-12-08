df9b6117621bce0148af9e77d53dabde6e234062
Memo Doring <memodoring@users.noreply.github.com>
2022-12-07 17:52:09 -0800

    add input file

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    if let Ok(lines) =  read_lines(file_path){
+        if let Ok(input) = line {
+            println!("{}", input);
+        }
+    }
+}
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
+where P: AsRef<Path>,{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
